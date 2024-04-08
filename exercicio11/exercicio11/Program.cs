e5e905c57daed8af519f2d0b03f6233699dda787
RodrigoPaivaRocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-04-08 15:36:23 -0300

    Add files via upload

---
+++ exercicio11/exercicio11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorX,valorY,resultado;
+
+            Console.WriteLine("Digite o valor da base: ");
+            valorX = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor do expoente: ");
+            valorY = double.Parse(Console.ReadLine());
+
+            resultado = Math.Pow(valorX, valorY);
+
+            Console.WriteLine("O valor da base elevada ao expoente é: " + resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
