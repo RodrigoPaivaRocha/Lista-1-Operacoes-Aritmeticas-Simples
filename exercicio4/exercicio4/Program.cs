e94313e3868074d1c1365a4c734235539bb803c6
RodrigoPaivaRocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-04-08 15:26:29 -0300

    Add files via upload

---
+++ exercicio4/exercicio4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseTriang, alturaTriang;
+            double area;
+
+            Console.WriteLine("Digite o valor da base do triângulo: ");
+            baseTriang = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor da altura do triângulo: ");
+            alturaTriang = int.Parse(Console.ReadLine());
+
+            area = (baseTriang *  alturaTriang)/2;
+
+            Console.WriteLine("O valor da sua área é: " + area);
+
+            Console.ReadKey();
+        }
+    }
+}
