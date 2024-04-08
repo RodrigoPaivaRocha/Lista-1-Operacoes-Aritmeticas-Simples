14fd061de1318656f4df246da200b1a2997e3909
RodrigoPaivaRocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-04-08 15:31:01 -0300

    Add files via upload

---
+++ exercicio8/exercicio8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorCelsius, valorFahrenheit;
+
+            Console.WriteLine("Digite a temperatura em celsius: ");
+            valorCelsius = double.Parse(Console.ReadLine());
+
+            valorFahrenheit = (valorCelsius * 1.8) + 32;
+
+            Console.WriteLine("A temperatura de Celsius em Fahrenheit é: " + valorFahrenheit);
+
+            Console.ReadKey();
+        }
+    }
+}
