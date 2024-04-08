44bb9a91f68173afccdd21b03b61f7170f75082d
RodrigoPaivaRocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-04-08 15:29:52 -0300

    Add files via upload

---
+++ exercicio7/exercicio7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double milhasMarit,resultado;
+
+            Console.WriteLine("Digite a quantidade de milhas marítimas para converter para quilômetros: ");
+            milhasMarit = double.Parse(Console.ReadLine());
+
+            resultado = milhasMarit * 1852;
+
+            Console.WriteLine("A quantidade de milhas marítimas em quilômetros é: " + resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
