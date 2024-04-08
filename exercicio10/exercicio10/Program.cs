c2f42148effe5d492db6c7f33c678a49d6540331
RodrigoPaivaRocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-04-08 15:33:35 -0300

    Add files via upload

---
+++ exercicio10/exercicio10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cotacaoDol, valorDol, valorReal;
+
+            Console.WriteLine("Digite o valor da cotação do dólar: ");
+            cotacaoDol = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma quantidade de dólares: ");
+            valorDol = double.Parse(Console.ReadLine());
+
+            valorReal = valorDol * cotacaoDol;
+
+            Console.WriteLine("O valor da quantidade de dólares digitada em real é: " + valorReal);
+
+            Console.ReadKey();
+        }
+    }
+}
