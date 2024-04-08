80ad956a0d51158f6ff04fbefcc88f1ab1069206
RodrigoPaivaRocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-04-08 15:24:46 -0300

    Add files via upload

---
+++ exercicio2/exercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int aresta;
+
+            Console.WriteLine("Digite o valor da aresta: ");
+            aresta = int.Parse(Console.ReadLine());
+
+            double area = Math.Pow(aresta,2);
+
+            Console.WriteLine("O valor da sua área é: " + area);
+
+            Console.ReadKey();
+        }
+    }
+}
